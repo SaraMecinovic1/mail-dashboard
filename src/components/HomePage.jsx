341306bbbd09b84e20c9231d16a81362bdf3e489
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-30 12:41:46 +0200

    card is done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,13 @@
 import Title from "../ui/Title";
+import CardInputs from "../ui/CardInputs";
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto flex items-center justify-center h-[100vh]">
+    <div className="container mx-auto flex flex-col items-center justify-center h-[100vh] space-y-4">
       <Title />
-
+      <CardInputs />
     </div>
   );
-}; 
+};
 
 export default HomePage;
--- src/ui/CardInputs.jsx
+++ src/ui/CardInputs.jsx
@@ -1,11 +1,45 @@
-
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
 
 const CardInputs = () => {
   return (
-  <div>
-    
-  </div>
-  )
+    <div className="w-80 h-[280px] border rounded-md border-slate-400 flex items-center justify-center ">
+      <FormControl className="flex flex-col items-center">
+        <FormLabel>Email:</FormLabel>
+        <Input
+          sx={{
+            width: "270px",
+            height: "45px",
+            marginBottom: "20px",
+          }}
+        />
+
+        <FormLabel>Referal code:</FormLabel>
+        <Input
+          sx={{
+            width: "270px",
+            height: "45px",
+            marginBottom: "10px",
+          }}
+        />
+
+        <Button
+          sx={{
+            width: "200px",
+            marginTop: "20px",
+            backgroundColor: "#e6d39a",
+            "&:hover": {
+              backgroundColor: "#d5b886", // tamnija boja za hover
+            },
+          }}
+        >
+          Send
+        </Button>
+      </FormControl>
+    </div>
+  );
 };
 
 export default CardInputs;
