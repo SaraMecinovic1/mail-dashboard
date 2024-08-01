b48bb441db63e97dca7225cb730e5ab939e45053
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-08-01 14:40:24 +0200

    fetching datas...

--- src/components/DataPage.jsx
+++ src/components/DataPage.jsx
@@ -2,11 +2,36 @@ import DataTitle from "../ui/DataTitle";
 import Table from "@mui/joy/Table";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
 
 const DataPage = () => {
+  const [emails, setEmails] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    const fetchEmails = async () => {
+      const { data, error } = await supabase.from("emails").select();
+
+      if (error) {
+        setFetchError("Could not fetch the emails!");
+        setEmails(null);
+        console.log("fetch error-", error);
+      } else {
+        setEmails(data);
+        setFetchError(null);
+        console.log("data-", data);
+      }
+    };
+    fetchEmails();
+  }, []);
+
   return (
     <div className="container mx-auto flex flex-col h-[100vh] space-y-6">
       <DataTitle />
+      {fetchError && (
+        <p style={{ color: "red", textAlign: "center" }}>{fetchError}</p>
+      )}
       <div className="overflow-x-auto">
         <Table aria-label="table variants" sx={{ borderCollapse: "collapse" }}>
           <thead>
@@ -19,7 +44,7 @@ const DataPage = () => {
             >
               <th
                 style={{
-                  minWidth: "200px", 
+                  minWidth: "200px",
                   fontSize: "18px",
                   color: "#595656",
                   paddingLeft: "20px",
@@ -27,39 +52,57 @@ const DataPage = () => {
               >
                 Email:
               </th>
-              <th style={{ minWidth: "100px", fontSize: "18px", color: "#595656" }}>
+              <th
+                style={{
+                  minWidth: "100px",
+                  fontSize: "18px",
+                  color: "#595656",
+                }}
+              >
                 Code:
               </th>
-              <th style={{ minWidth: "100px", fontSize: "18px", color: "#595656" }}>
+              <th
+                style={{
+                  minWidth: "100px",
+                  fontSize: "18px",
+                  color: "#595656",
+                }}
+              >
                 Id:
               </th>
-              <th style={{ minWidth: "150px", fontSize: "18px", color: "#595656" }}>
+              <th
+                style={{
+                  minWidth: "150px",
+                  fontSize: "18px",
+                  color: "#595656",
+                }}
+              >
                 Sent:
               </th>
               <th style={{ minWidth: "150px" }}>*Edit data:</th>
             </tr>
           </thead>
           <tbody>
-            <tr style={{ borderBottom: "2px solid #e6d38a" }}>
-              <td style={{ paddingLeft: "20px" }}>saramecinovic</td>
-              <td>1234</td>
-              <td>56</td>
-              <td>12 12 2006</td>
-              <td style={{ paddingLeft: "15px" }}>
-                <EditIcon sx={{ paddingRight: "10px", fontSize: "33px" }} />
-                <DeleteIcon />
-              </td>
-            </tr>
-            <tr style={{ borderBottom: "2px solid #e6d38a" }}>
-              <td style={{ paddingLeft: "20px" }}>saramecinovic</td>
-              <td>1234</td>
-              <td>56</td>
-              <td>12 12 2006</td>
-              <td style={{ paddingLeft: "15px" }}>
-                <EditIcon sx={{ paddingRight: "10px", fontSize: "33px" }} />
-                <DeleteIcon />
-              </td>
-            </tr>
+            {emails && emails.length > 0 ? (
+              emails.map((email, index) => (
+                <tr key={index} style={{ borderBottom: "2px solid #e6d38a" }}>
+                  <td style={{ paddingLeft: "20px" }}>{email.email}</td>
+                  <td>{email.code}</td>
+                  <td>{email.id}</td>
+                  <td>{email.created_at}</td>
+                  <td style={{ paddingLeft: "15px" }}>
+                    <EditIcon sx={{ paddingRight: "10px", fontSize: "33px" }} />
+                    <DeleteIcon />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5" style={{ textAlign: "center" }}>
+                  No data available
+                </td>
+              </tr>
+            )}
           </tbody>
         </Table>
       </div>
