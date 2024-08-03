6efe16f5f3ce4d3c895a7026140e1f196c9b0ec6
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-08-03 21:18:51 +0200

    login ui

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+import LoginTitle from "../ui/LoginTitle";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/joy/Button";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center h-[100vh] space-y-6">
+      <LoginTitle />
+      <div className="w-80 flex-col h-[285px] border rounded-md border-slate-400 flex items-center justify-center mt-8">
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Button
+          sx={{
+            color: "#595656",
+            width: "200px",
+            marginTop: "20px",
+            backgroundColor: "#e6d39a",
+            "&:hover": {
+              backgroundColor: "#d5b886",
+            },
+          }}
+        >
+          Send
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/ui/LoginTitle.jsx
@@ -0,0 +1,7 @@
+const LoginTitle = () => {
+  return (
+    <h1 className="font-sans text-3xl text-[#595656]	">Welcome back, boss!</h1>
+  );
+};
+
+export default LoginTitle;
